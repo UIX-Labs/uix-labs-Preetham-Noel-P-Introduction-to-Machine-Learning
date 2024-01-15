0d3180a6b2c2b0fae03474e1c1e89f26a3382caf
Preetham Noel P <preethamnoel.05@gmail.com>
2024-01-15 12:49:02 +0530

    task: Task: Build a Popularity Based Recommendation System

--- recommenders/popularity_recommender.py
+++ recommenders/popularity_recommender.py
@@ -2,16 +2,9 @@ from recommenders import data_movies, data_ratings, default_poster_url
 import pandas as pd
 
 
-def recommend_popular_movies():
-
-    ######################################################################################################
-    movie_ratings = None
-    
-    merged_data = pd.merge(data_movies, data_ratings, on='movieId')
-    movie_ratings = merged_data.groupby('title')['rating'].mean().sort_values(ascending=False).head(20).to_dict()
-    #######################################################################################################
-    response = []
-
+def recommend_popular_movies():    
+    movies_ratings_merged = pd.merge(data_movies, data_ratings, on='movieId')
+    movie_ratings = movies_ratings_merged.groupby('title')['rating'].mean().sort_values(ascending=False).head(20).to_dict()
     for movie in movie_ratings:
         movie_record = data_movies[data_movies.title == movie].iloc[0]
         movie_poster = None
@@ -22,6 +15,4 @@ def recommend_popular_movies():
             "genres": str(movie_record.genres).split("|"),
             "average_rating": movie_ratings[movie]
         })
-
-
     return {"status": True, "data": {"message": "Here are some of the popular recommendations.", "results" : response}}
