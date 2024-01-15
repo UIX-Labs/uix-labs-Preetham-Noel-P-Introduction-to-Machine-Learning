62c8ead0318e4612ddde5c29dda62b1d84289802
Preetham Noel P <preethamnoel.05@gmail.com>
2024-01-15 12:56:03 +0530

    task: Task: Coding Question

--- codingQuestion.py
+++ codingQuestion.py
@@ -1,1 +1,13 @@
+L = int(input())
+N = int(input())
+
+for _ in range(N):
+    W, H = map(int, input().split())
+
+    if W < L or H < L:
+        print("UPLOAD ANOTHER")
+    elif W == H:
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
 
