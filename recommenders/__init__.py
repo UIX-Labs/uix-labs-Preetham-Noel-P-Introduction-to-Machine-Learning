426b419333a6380a9d285c987deeebefc587ae1e
Preetham Noel P <preethamnoel.05@gmail.com>
2024-01-15 12:38:20 +0530

    task: Task: Load Datasets for Recommendation System

--- recommenders/__init__.py
+++ recommenders/__init__.py
@@ -8,26 +8,14 @@ default_poster_url = "https://gurukul-be.s3.ap-south-1.amazonaws.com/0c3df9f4dc5
 
 ####################################################################################################################################
 
-'''
-Complete the code below
 
-Things to do: load csv in dataframes
+data_ratings =pd.read_csv("data_files/ratings.csv") 
+data_movies = pd.read_csv( "data_files/movies.csv") 
+data_posters = pd.read_csv("data_files/movie_poster.csv") 
 
-data_ratings => file path : 'data_files/ratings.csv'
-data_movies => file path : 'data_files/movies.csv'
-data_posters => file path : 'data_files/movie_poster.csv'
-
-'''
-data_ratings =None # empty df
-data_movies = None # empty df
-data_posters = None # empty df
-
-
+####################################################################################################################################
 '''
 Uncomment the below line after you complete the code above.
 '''
-#data_movies = pd.merge(data_movies, data_posters, how="left", on="movieId")
-
+data_movies = pd.merge(data_movies, data_posters, how="left", on="movieId")
 #####################################################################################################################################
-
-
